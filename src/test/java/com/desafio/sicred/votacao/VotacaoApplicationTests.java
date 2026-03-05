0650a643ec4dad6abd5c10865ff5734851be0597
Sidney Victor <sidneyvictormml@gmail.com>
2026-03-04 21:34:53 -0300

    project-base

---
+++ src/test/java/com/desafio/sicred/votacao/VotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.sicred.votacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
